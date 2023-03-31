acc1dd53958a8bb82c617e1f8e50f70435c7ffa1
brillandthomas <brillandthomas@gmail.com>
2023-03-31 11:06:57 +0200

    6.2 question 6

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -1,15 +1,19 @@
-from communication.message.Message import Message
-from communication.message.MessagePerformative import MessagePerformative
+from copy import deepcopy
+import random
 from mesa import Model
 from mesa.time import RandomActivation
 
 from communication.agent.CommunicatingAgent import CommunicatingAgent
+
 from communication.message.MessageService import MessageService
+from communication.message.Message import Message
+from communication.message.MessagePerformative import MessagePerformative
+
 from communication.preferences.Preferences import Preferences
 from communication.preferences.Item import Item
 from communication.preferences.CriterionName import CriterionName
 from communication.preferences.CriterionValue import CriterionValue
-import random
+
 
 class ArgumentAgent(CommunicatingAgent) :
     """ ArgumentAgent which inherit from CommunicatingAgent .
@@ -17,22 +21,39 @@ class ArgumentAgent(CommunicatingAgent) :
     def __init__(self, unique_id, model, name, preferences) :
         super().__init__ (unique_id, model, name)
         self.preference = preferences
+        self.list_items = [item for item in self.model.items]
 
     def step(self) :
         super().step()
-        # print(self.get_name() + " : "+ str(len(self.get_new_messages())) + " new messages")
-        for message in self.get_new_messages():
-            # print(message.get_performative())
-            if message.get_performative() == MessagePerformative.PROPOSE:
-                item = message.get_content()
+        list_messages = self.get_new_messages()
+        # print(self.get_name() + " : "+ str(len(list_messages)) + " new messages")
+        for message in list_messages:
+            item = message.get_content()
+            performative = message.get_performative()
+            if performative == MessagePerformative.PROPOSE:
+
                 if self.preference.is_item_among_top_10_percent(item, self.model.items):
                     message_to_send = Message(self.get_name(), message.get_exp(), MessagePerformative.ACCEPT, item)
                     self.send_message(message_to_send)
-                    # print(self.get_name() + " : accept item")
+                    print(self.get_name() + " : accept item")
                 else:
                     message_to_send = Message(self.get_name(), message.get_exp(), MessagePerformative.ASK_WHY, item)
                     self.send_message(message_to_send)
-                    # print(self.get_name() + " : why ?")
+                    print(self.get_name() + " : why ?")
+
+            elif performative == MessagePerformative.ACCEPT and item in self.list_items:
+                self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.COMMIT, item))
+                self.list_items.remove(item)
+                print(self.get_name() + " : commit")
+            
+            elif performative == MessagePerformative.COMMIT and item in self.list_items:
+                self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.COMMIT, item))
+                self.list_items.remove(item)
+                print(self.get_name() + " : commit")
+            
+            else:
+                self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ARGUE, None))
+                print(self.get_name() + " : argue")
 
     def get_preference(self):
         return self.preference
@@ -54,7 +75,7 @@ class ArgumentModel(Model) :
         self.items = [Item("ICED", "ICE Diesel Engine"), Item("E", "Electric Engine")]
         
         #Generate agents
-        for i in range(2): #we can modify this later to handle n agents
+        for i in range(1,3): #we can modify this later to handle n agents
             preferences = Preferences()
             agent = ArgumentAgent(i, self, "Agent"+str(i), preferences)
             agent.generate_preferences(self.items)
@@ -63,7 +84,7 @@ class ArgumentModel(Model) :
 
     def step(self):
         self.__messages_service.dispatch_messages()
-        self.schedule.step ()
+        self.schedule.step()
 
 if __name__ == "__main__":
     argument_model = ArgumentModel()
@@ -73,7 +94,7 @@ if __name__ == "__main__":
     proposed_item = random.choice(argument_model.items)
     message = Message(agent1.get_name(), agent2.get_name(), MessagePerformative.PROPOSE, proposed_item)
     agent1.send_message(message)
-    # print("Agent1 to Agent2: ", proposed_item.get_name())
+    print("Agent1 to Agent2: ", proposed_item.get_name())
 
     step = 0
     while step < 10:
