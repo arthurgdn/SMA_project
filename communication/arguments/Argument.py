ab2d73a85cbe6f3f49a04f2ab907c0e385da9041
brillandthomas <brillandthomas@gmail.com>
2023-03-31 11:14:56 +0200

    arguments init

---
+++ communication/arguments/Argument.py
@@ -0,0 +1,31 @@
+# !/ usr / bin / env python3
+
+from communication.arguments.Comparison import Comparison
+from communication.arguments.CoupleValue import CoupleValue
+
+
+class Argument:
+    """Argument class.
+    This class implements an argument used during the interaction.
+    
+    attr:
+        decision:
+        item:
+        comparison_list:
+        couple_values_list:
+    """
+
+    def __init__(self, boolean_decision, item):
+        """Creates a new Argument.
+        """
+        #To be completed
+    
+    def add_premiss_comparison(self, criterion_name_1, criterion_name_2):
+        """Adds a premiss comparison in the comparison list.
+        """
+        #To be completed
+    
+    def add_premiss_couple_values(self, criterion_name, value):
+        """Adds a premis couple values in the couple values list.
+        """
+        #To be completed
