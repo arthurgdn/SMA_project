f1e912fcc5489c3fbbe35cba1b89cb59e74ec673
brillandthomas <brillandthomas@gmail.com>
2023-03-31 11:28:38 +0200

    argument question 4

--- communication/arguments/Argument.py
+++ communication/arguments/Argument.py
@@ -18,14 +18,17 @@ class Argument:
     def __init__(self, boolean_decision, item):
         """Creates a new Argument.
         """
-        #To be completed
+        self.decision = boolean_decision
+        self.item = item
+        self.comparison_list = []
+        self.couple_values_list = []
     
     def add_premiss_comparison(self, criterion_name_1, criterion_name_2):
         """Adds a premiss comparison in the comparison list.
         """
-        #To be completed
+        self.comparison_list.append(Comparison(criterion_name_1, criterion_name_2))
     
     def add_premiss_couple_values(self, criterion_name, value):
         """Adds a premis couple values in the couple values list.
         """
-        #To be completed
+        self.couple_values_list.append(CoupleValue(criterion_name, value))
