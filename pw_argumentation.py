08437d5994738af15a9599a56ab626a56696dd6c
brillandthomas <brillandthomas@gmail.com>
2023-03-31 10:36:06 +0200

    correction bugs

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -7,31 +7,32 @@ from communication.agent.CommunicatingAgent import CommunicatingAgent
 from communication.message.MessageService import MessageService
 from communication.preferences.Preferences import Preferences
 from communication.preferences.Item import Item
-from communication.preferences import CriterionName
-from communication.preferences import CriterionValue
+from communication.preferences.CriterionName import CriterionName
+from communication.preferences.CriterionValue import CriterionValue
 import random
 
 class ArgumentAgent(CommunicatingAgent) :
     """ ArgumentAgent which inherit from CommunicatingAgent .
     """
     def __init__(self, unique_id, model, name, preferences) :
-        super().__init__ (unique_id, model, name, preferences)
+        super().__init__ (unique_id, model, name)
         self.preference = preferences
 
     def step(self) :
         super().step()
-        print(self.get_name() + " : "+ len(self.get_new_messages()) + " new messages")
+        # print(self.get_name() + " : "+ str(len(self.get_new_messages())) + " new messages")
         for message in self.get_new_messages():
+            # print(message.get_performative())
             if message.get_performative() == MessagePerformative.PROPOSE:
                 item = message.get_content()
                 if self.preference.is_item_among_top_10_percent(item, self.model.items):
                     message_to_send = Message(self.get_name(), message.get_exp(), MessagePerformative.ACCEPT, item)
                     self.send_message(message_to_send)
-                    print(self.get_name() + " : accept item")
+                    # print(self.get_name() + " : accept item")
                 else:
                     message_to_send = Message(self.get_name(), message.get_exp(), MessagePerformative.ASK_WHY, item)
                     self.send_message(message_to_send)
-                    print(self.get_name() + " : why ?")
+                    # print(self.get_name() + " : why ?")
 
     def get_preference(self):
         return self.preference
@@ -64,8 +65,7 @@ class ArgumentModel(Model) :
         self.__messages_service.dispatch_messages()
         self.schedule.step ()
 
-if __name__ == " __main__ ":
-    print("test")
+if __name__ == "__main__":
     argument_model = ArgumentModel()
     agent1 = argument_model.schedule.agents[0]
     agent2 = argument_model.schedule.agents[1]
@@ -73,7 +73,7 @@ if __name__ == " __main__ ":
     proposed_item = random.choice(argument_model.items)
     message = Message(agent1.get_name(), agent2.get_name(), MessagePerformative.PROPOSE, proposed_item)
     agent1.send_message(message)
-    print("Agent1 to Agent2: ", proposed_item.get_name())
+    # print("Agent1 to Agent2: ", proposed_item.get_name())
 
     step = 0
     while step < 10:
